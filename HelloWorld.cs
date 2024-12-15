c717839c948b88f4d743d0e92319f4fc17dcf38d
VZ-Algo <garesnica@algorithmicschool.com>
2024-12-15 09:46:05 +0100

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //This is a comment!
+        print("Hello, World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
